d2cf293ec537920e09b802edc2e8ed013095fc6a
abdullahmansuri267 <abdullahmansuri267@gmail.com>
2023-06-23 11:51:47 +0500

    First Dart Assignment

---
+++ assigment 1.dart
@@ -0,0 +1,200 @@
+void main() {
+  // Question:1
+
+  // int length = 5;
+  // int breadth = 5;
+  // if (length == breadth) {
+  //   print("square");
+  // } else {
+  //   print("rectangle");
+  // }
+
+  // Question:2
+
+  // int num1 = 20;
+  // int num2 = 25;
+  // if (num1 > num2) {
+  //   print("Old: $num1");
+  //   print("Young: $num2");
+  // } else {
+  //   print("Old: $num2");
+  //   print("Young: $num1");
+  // }
+
+  // Question:3
+
+  // int num1 = 16;
+  // int num2 = 10;
+  // num num3 = (num2 / num1) * 100;
+  // print("Percentage of classes attend:{$num3}%");
+  // bool num4 = num3 >= 75;
+  // if (num4) {
+  //   print("You are allowed to sit in exam");
+  // } else {
+  //   print("You are not allow to sit in exam");
+  // }
+
+  // Question:4
+
+  // int year = ;
+
+  // if (year % 4 == 0) {
+  //   if (year % 100 == 0) {
+  //     if (year % 400 == 0) {
+  //       print("$year is a leap year");
+  //     } else {
+  //       print("$year is not a leap year");
+  //     }
+  //   } else {
+  //     print("$year is a leap year");
+  //   }
+  // } else {
+  //   print("$year is not a leap year");
+  // }
+
+  // Question 5
+
+  // var temprature = ;
+
+  // if (temprature < 0) {
+  //   print("freezing-Temprature");
+  // } else if (temprature >= 0 && temprature < 10) {
+  //   print("very cold-Temprature");
+  // } else if (temprature >= 10 && temprature < 20) {
+  //   print("cold-Temprature");
+  // } else if (temprature >= 20 && temprature < 30) {
+  //   print("normal-Temprature");
+  // } else if (temprature >= 30 && temprature < 40) {
+  //   print("its Hot-Temprature");
+  // } else {
+  //   print("its Very hot-Temprature");
+  // }
+
+  // Question 6
+
+  // String num1 = "a";
+  // List<String> vowels = ["a", "e", "i", "o", "u"];
+
+  // bool isVowel = vowels.contains(num1);
+  // if (isVowel) {
+  //   print("The letter '$num1' is a vowel.");
+  // } else {
+  //   print("The letter '$num1' is a consonant.");
+  // }
+
+  // Question 7
+
+  //  String id = "" ;
+  // String name = "";
+  // int units = ;
+  // double chargePerUnit = 0;
+  // if (units <= 199) {
+  //   chargePerUnit = 1.20;
+  // } else if (units > 199 && units <= 400) {
+  //   chargePerUnit = 1.50;
+  // } else if (units > 400 && units <= 600) {
+  //   chargePerUnit = 1.80;
+  // } else {
+  //   chargePerUnit = 2.00;
+  // }
+  // double billAmount = units * chargePerUnit;
+  // print("Customer IDNO: $id");
+  // print("Customer Name: $name");
+  // print("Unit Consumed: $units");
+  // print("Amount Charges @Rs. $chargePerUnit per unit: $billAmount");
+  // print("Net Bill Amount: $billAmount");
+
+  // Question 8
+
+  // String name = "";
+  // String id = "";
+  // String clas = "";
+
+  // int english = 95;
+  // int urdu = 90;
+  // int maths = 80;
+  // int islamiat = 88;
+  // int science = 67;
+  // num num1 = english + urdu + maths + islamiat + science;
+  // num num2 = 500;
+
+  // String grade = "0";
+  // if (num1 <= 250) {
+  //   grade = "Fail";
+  // } else if (num1 > 250 && num1 <= 300) {
+  //   grade = "D";
+  // } else if (num1 > 300 && num1 <= 350) {
+  //   grade = "C";
+  // } else if (num1 > 350 && num1 <= 400) {
+  //   grade = "B";
+  // } else if (num1 > 400 && num1 <= 450) {
+  //   grade = "A";
+  // } else {
+  //   grade = "A+";
+  // }
+
+  // double percentage = (num1 / num2) * 100;
+  // print("Student name :$name");
+  // print("Student Id :$id");
+  // print("Class :$clas");
+  // print("Percentage :$percentage%");
+  // print("Grade :$grade");
+
+  // Question 9
+
+  // var number = 10;
+
+  // bool isEven = number % 2 == 0;
+
+  // if (isEven) {
+  //   bool isDivisibleBy5 = number % 5 == 0;
+  //   print("The number is even and divisible by 5: $isDivisibleBy5");
+  // } else {
+  //   bool isDivisibleBy7 = number % 7 == 0;
+  //   print("The number is odd and divisible by 7: $isDivisibleBy7");
+  // }
+  // print("Enter a number: ");
+
+  // Question 10
+
+  // var num1 = 5;
+  // var num2 = 6;
+  // var num3 = 7;
+
+  // int greatestNumber = num1;
+  // if (num2 > greatestNumber) {
+  //   greatestNumber = num2;
+  // }
+  // if (num3 > greatestNumber) {
+  //   greatestNumber = num3;
+  // }
+  // int lowestNumber = num1;
+  // if (num2 < lowestNumber) {
+  //   lowestNumber = num2;
+  // }
+  // if (num3 < lowestNumber) {
+  //   lowestNumber = num3;
+  // }
+  // print('The greatest number is $greatestNumber');
+  // print('The lowest number is $lowestNumber');
+  // print("Enter three numbers: ");
+
+// Question 11
+
+//  import "dart:math";
+
+// void main() {
+//   // Get the number from the user
+//   double number = ;
+//   double root =sqrt (number);
+//   print('The square root of $number is $root');
+// }
+// Question 12
+
+  // String input = "";
+  // double celsius = double.parse(input);
+
+  // double fahrenheit = (celsius * 9 / 5) + 32;
+  // print("The temperature in Fahrenheit is $fahrenheit F");
+  // print("Enter the temperature in Celsius: $input C");
+}
